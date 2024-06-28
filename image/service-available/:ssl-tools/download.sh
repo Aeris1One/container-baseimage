a0ae1ba1cedd43234b2859efdf82eeea5db331cb
Charles P <aeris@e.email>
2024-06-28 21:18:04 +0200

    update cfssl to 1.6.5

--- image/service-available/:ssl-tools/download.sh
+++ image/service-available/:ssl-tools/download.sh
@@ -52,13 +52,13 @@ if [[ "${HOST_ARCH}" == 'arm' ]]; then
 fi
 
 echo "Download cfssl ..."
-echo "curl -o /usr/sbin/cfssl -SL https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssl_1.6.4_linux_${HOST_ARCH}"
-curl -o /usr/sbin/cfssl -SL "https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssl_1.6.4_linux_${HOST_ARCH}"
+echo "curl -o /usr/sbin/cfssl -SL https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssl_1.6.5_linux_${HOST_ARCH}"
+curl -o /usr/sbin/cfssl -SL "https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssl_1.6.5_linux_${HOST_ARCH}"
 chmod 700 /usr/sbin/cfssl
 
 echo "Download cfssljson ..."
-echo "curl -o /usr/sbin/cfssljson -SL https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssljson_1.6.4_linux_${HOST_ARCH}"
-curl -o /usr/sbin/cfssljson -SL "https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssljson_1.6.4_linux_${HOST_ARCH}"
+echo "curl -o /usr/sbin/cfssljson -SL https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssljson_1.6.5_linux_${HOST_ARCH}"
+curl -o /usr/sbin/cfssljson -SL "https://github.com/cloudflare/cfssl/releases/download/v1.6.5/cfssljson_1.6.5_linux_${HOST_ARCH}"
 chmod 700 /usr/sbin/cfssljson
 
 echo "Project sources: https://github.com/cloudflare/cfssl"
